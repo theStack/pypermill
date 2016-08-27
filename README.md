c91378c3f460a0c8be5fc7d075cfceee2ed9fb27
theStack <sebastian.falbesoner@gmail.com>
2016-08-27 10:19:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# pypermill
+
+*pypermill* is a simple standalone Python (3.X) clone of
+[factoidpipermill](github.com/FactomProject/factoidpapermill)
+which creates a new Factoid keypair in human readable format.
+
+written by Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
+
+Bitcoin donation address: 1KPs499E4H6HK3ZQz565nPgQh3zLwNWDdB
+
+## TODO
+* also support Python 2.X (concerns only Base58 function so far)
+* create neat-looking HTML output with QR codes (deserving the name "paper wallet")
---
+++ factoidpypermill.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# pypermill -- simple Factom paper wallet generator written in Python
+from os import urandom
+from hashlib import sha256
+import ed25519
+
+FactoidPrefix = b'\x5f\xb1'
+FactoidPrivatePrefix = b'\x64\x78'
+
+def SHA256D(bstr):
+    return sha256(sha256(bstr).digest()).digest()
+
+def Base58(bstr):
+    alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    num = 0
+    p = 1
+    for b in bstr[::-1]:
+        num += p*b
+        p <<= 8
+
+    result = ''
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result += alphabet[mod]
+
+    return result[::-1]
+
+def ConvertAddressToUserStr(prefix, addr):
+    data = prefix + addr
+    return Base58(data + SHA256D(data)[:4])
+
+# generate private key
+privatekey = urandom(32)
+privaddr = ConvertAddressToUserStr(FactoidPrivatePrefix, privatekey)
+print ("New Factoid Private Key: %s" % privaddr)
+
+# determine public key
+publickey = ed25519.SigningKey(privatekey).get_verifying_key().to_bytes()
+# convert to RCD_1 address format (simple signature)
+publickey = b'\x01' + publickey
+publickey = SHA256D(publickey)
+publicaddr = ConvertAddressToUserStr(FactoidPrefix, publickey)
+print ("New Factoid Address:     %s" % publicaddr)
